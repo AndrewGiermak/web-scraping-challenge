bab007e50dc18bcc0bfe4120e1c2992964518f91
Andrew Giermak <64851573+AndrewGiermak@users.noreply.github.com>
2020-08-25 11:10:03 -0400

    Update app.py

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,40 +1,30 @@
-from flask import Flask, render_template, redirect, request
-from flask_pymongo import PyMongo
+from flask import Flask, jsonify
 import scrape_mars
-from pymongo import MongoClient
 import pymongo
-import os
+
+conn = 'mongodb://localhost:27017'
+client = pymongo.MongoClient(conn)
+db = client.mars_dict
 
 # create an instance of Flask
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
-mongo = PyMongo(app)
-
-# Create route that renders index.html template and finds documents from mongo
 @app.route("/")
-def home(): 
-    # find data in dict
-    mars_dict = mongo.db.mars_dict.find_one()
-    # Return template and data
-    return render_template("index.html", mars_dict=mars_dict)
+def welcome():
+    mars_dict = db.mars_dict.find()
+    # List all available api routes.
+    return (
+        f"Available Routes:<br/>"
+        f"/scrape"
+    )
 
-# route for the scrape functions
+
 @app.route("/scrape")
-def scrape(): 
-    # run functions
-    mars_dict = mongo.db.mars_dict
-    mars_web = scrape_mars.scrape_news()
-    mars_web = scrape_mars.scrape_image()
-    mars_web = scrape_mars.scrape_facts()
-    mars_web = scrape_mars.scrape_hemispheres()
+def web_scraper():
+    scrape_mars.scrape();
+    db.mars_dict.insert(scrape_mars.scrape())
+    return jsonify(scrape_mars.scrape())
 
-    # Update the Mongo Database using update and upsert=True
-    mars.update({}, mars_web, upsert=True)
 
-    # Redirect back to home page
-    return redirect("/", code=302)
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
-
