fb816d45f3d6ba635b00cb041e1577adb79868e2
Andrew Giermak <64851573+AndrewGiermak@users.noreply.github.com>
2020-08-26 10:53:56 -0400

    Add files via upload

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,31 +1,37 @@
 from flask import Flask, jsonify, render_template, redirect 
 import scrape_mars
-import pymongo
+from flask_pymongo import PyMongo
 import os
-
-conn = 'mongodb://localhost:27017'
-client = pymongo.MongoClient(conn)
-db = client.mars_dict
 
 # create an instance of Flask
 app = Flask(__name__)
 
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+# Run the scrape function
+
 @app.route("/")
 def welcome():
-    mars_dict = db.mars_dict.find()
-    # List all available api routes.
-    return (
-        f"Available Routes:<br/>"
-        f"/scrape"
-    )
+    mars_dict = mongo.db.mars_dict.find_one()
+    return render_template("index.html", mars_dict=mars_dict)
 
+# Run the scrape function
 
 @app.route("/scrape")
-def web_scraper():
-    scrape_mars.scrape();
-    db.mars_dict.insert(scrape_mars.scrape())
-    return jsonify(scrape_mars.scrape())
+def scraper():
+    mars_dict = mongo.db.mars_dict
+    mars_dict_data = scrape_mars.scrape_news()
+    mars_dict_data = scrape_mars.scrape_image()
+    mars_dict_data = scrape_mars.scrape_facts()
+    mars_dict_data = scrape_mars.scrape_hemispheres()
 
+    # Update the Mongo Database using update and upsert=True
+    mars_dict.update({}, mars_dict_data, upsert=True)
+
+    # Redirect back to home page
+    return redirect("/", code=302)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -6,11 +6,10 @@ import os
 import time
 import requests
 import warnings
-warnings.filterwarnings('ignore')
 
 def init_browser():
-    executable_path = {'executable_path': 'chromedriver.exe'}
-    browser = Browser('chrome', executable_path, headless=False)
+    executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
+    return Browser("chrome", **executable_path, headless=False)
 
 #create global dictionary 
 mars_dict = {}
@@ -21,20 +20,18 @@ mars_dict = {}
 def scrape_news():
     browser = init_browser()
 
-    url = 'https://mars.nasa.gov/news/'
+    url = "https://mars.nasa.gov/news/"
     browser.visit(url)
     
     html = browser.html
-    soup = bs(html, 'html.parser')
+    soup = bs(html, "html.parser")
 
-    news_title = soup.find('div', class_= 'content_title').text
-    news_date = soup.find('div', class_= 'list_date').text
-    paragraph_text = soup.find('div', class_='article_teaser_body').text
+    news_title = soup.find("div", class_= "content_title").get_text()
+    paragraph_text = soup.find("div", class_="article_teaser_body").get_text()
 
     # put in mars_dict
-    mars_dict['news_title'] = news_title
-    mars_dict['news_date'] = news_date
-    mars_dict['paragraph_text'] = paragraph_text
+    mars_dict["news_title"] = news_title
+    mars_dict["paragraph_text"] = paragraph_text
 
     browser.quit()
     return mars_dict
@@ -42,17 +39,17 @@ def scrape_news():
 # featured image
 def scrape_image():
     browser = init_browser()
-    featured_image_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    featured_image_url = '"https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
     browser.visit(featured_image_url)
     html_image = browser.html
-    soup = bs(html_image, 'html.parser')
+    soup = bs(html_image, "html.parser")
 
-    featured_image_url  = soup.find('article')['style'].replace('background-image: url(','').replace(');', '')[1:-1]
-    main_url = 'https://www.jpl.nasa.gov'
+    featured_image_url  = soup.find("article")["style"].replace("background-image: url(','').replace(');', '')[1:-1]")
+    main_url = "https://www.jpl.nasa.gov"
     featured_image_url = main_url + featured_image_url
     featured_image_url
 
-    mars_dict['featured_image_url'] = featured_image_url
+    mars_dict["featured_image_url"] = featured_image_url
 
     browser.quit()
     return mars_dict
@@ -60,16 +57,16 @@ def scrape_image():
 # Mars Facts
 def scrape_facts():
     browser = init_browser()
-    facts_url = 'https://space-facts.com/mars/'
+    facts_url = "https://space-facts.com/mars/"
     browser.visit(facts_url)    
     html = browser.html
-    soup = bs(html, 'html.parser')
+    soup = bs(html, "html.parser")
 
     tables = pd.read_html(facts_url)
     mars_df = tables[1]
     html_table = (mars_df.to_html()).replace('\n', '')
 
-    mars_dict['html_table'] = html_table
+    mars_dict["html_table"] = html_table
 
     browser.quit()
     return mars_dict
@@ -77,24 +74,24 @@ def scrape_facts():
 # Hemispheres
 def scrape_hemispheres():
     browser = init_browser()
-    hemisphere_url='https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    hemisphere_url="https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
     browser.visit(hemisphere_url)
     html = browser.html
-    soup = bs(html, 'html.parser')
+    soup = bs(html, "html.parser")
 
-    items = soup.find_all('div', class_='item')
+    items = soup.find_all("div", class_="item")
     hem_image_urls = []
-    hem_main_url = 'https://astrogeology.usgs.gov'
+    hem_main_url = "https://astrogeology.usgs.gov"
     for item in items: 
-        title = item.find('h3').text
-        image_url = item.find('a', class_='itemLink product-item')['href']
+        title = item.find("h3").text
+        image_url = item.find("a", class_="itemLink product-item")["href"]
         browser.visit(hem_main_url + image_url)
         image_html = browser.html
-        soup = bs(image_html, 'html.parser')
-        image_url = hem_main_url + soup.find('img', class_='wide-image')['src']
+        soup = bs(image_html, "html.parser")
+        image_url = hem_main_url + soup.find("img", class_="wide-image")["src"]
         hem_image_urls.append({"Title" : title, "Image_URL" : image_url})
 
-    mars_dict['hem_image_urls'] = hem_image_urls
+    mars_dict["hem_image_urls"] = hem_image_urls
 
     browser.quit()
     return mars_dict
