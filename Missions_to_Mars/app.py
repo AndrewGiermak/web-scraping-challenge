fe01ea5bd5353f78b2089f8377009c68c57f9838
Andrew Giermak <64851573+AndrewGiermak@users.noreply.github.com>
2020-08-27 18:14:16 -0400

    Add files via upload

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -2,33 +2,32 @@ from flask import Flask, jsonify, render_template, redirect
 import scrape_mars
 from flask_pymongo import PyMongo
 import os
+import jinja2
 
 # create an instance of Flask
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars"
 mongo = PyMongo(app)
-
+      
 # Run the scrape function
-
 @app.route("/")
 def welcome():
     mars_dict = mongo.db.mars_dict.find_one()
-    return render_template("index.html", mars_dict=mars_dict)
+    return render_template("index.html", mars_dict = mars_dict)
 
 # Run the scrape function
-
 @app.route("/scrape")
-def scraper():
+def scrape():
     mars_dict = mongo.db.mars_dict
-    mars_dict_data = scrape_mars.scrape_news()
-    mars_dict_data = scrape_mars.scrape_image()
-    mars_dict_data = scrape_mars.scrape_facts()
-    mars_dict_data = scrape_mars.scrape_hemispheres()
+    mars_data = scrape_mars.scrape_news()
+    mars_data = scrape_mars.scrape_image()
+    mars_data = scrape_mars.scrape_facts()
+    mars_data = scrape_mars.scrape_hemispheres()
 
     # Update the Mongo Database using update and upsert=True
-    mars_dict.update({}, mars_dict_data, upsert=True)
+    mars_dict.update({}, mars_data, upsert=True)
 
     # Redirect back to home page
     return redirect("/", code=302)
